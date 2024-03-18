4daafc65fff9c73e9217fd8dab110b8aef9f72a2
maud-barthelemy <63283760+maud-barthelemy@users.noreply.github.com>
2024-03-18 10:31:39 +0100

    Change match case to dictionary

--- algos/__init__.py
+++ algos/__init__.py
@@ -1,3 +1,3 @@
 from .algo_map import get_algo_method
-from .algos_list import AVAILABLE_ALGOS, get_required_algo_params, get_algo_info
+from .algos_def import AVAILABLE_ALGOS, get_required_algo_params, get_algo_info
 from .compute_features import get_features_from_segm_mask
--- algos/algo_map.py
+++ algos/algo_map.py
@@ -2,12 +2,10 @@ from typing import Callable
 
 from .stardist_run import run_stardist
 
-
-# TODO: replace match/case by dict ? algos_map = {"stardist": run_stardist} (compatible with earlier Python versions)
+# This maps the algo name from algos_def.py to its callable method
+ALGOS_MAP = {"stardist": run_stardist}
 
 
 def get_algo_method(algo_name: str) -> Callable or None:
-    """ Map the algo_name to its Callable algo method"""
-    match algo_name:
-        case "stardist":
-            return run_stardist
+    """ Return the Callable algo method for the given algo name"""
+    return ALGOS_MAP.get(algo_name)
